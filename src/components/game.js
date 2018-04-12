6eeb917343a754689641d35f08fa38e9f542b6b2
ssheaks <sarah.sheaks@gmail.com>
2018-04-12 09:55:04 -0700

    user able to make guesses

--- src/components/game.js
+++ src/components/game.js
@@ -5,14 +5,31 @@ import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            guesses: []
+        }
+    }
+
+    makeGuess(number) {
+        this.setState({
+            guesses: [...this.state.guesses, number]
+        });
+    }
+
+    render() {
     return (
         <div>
             <Header />
-            <GuessSection feedback="Make your guess!" />
+            <GuessSection feedback="Make your guess!" add={(value) => this.makeGuess(value)} />
             <GuessCount count={3} />
-            <GuessList guesses={[10, 15, 25]} />
+            <GuessList guesses={this.state.guesses} />
         </div>
-    );
+        );
+    }
 }
+
+export default Game
 
--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -3,11 +3,19 @@ import React from 'react';
 import './guess-form.css';
 
 export default function GuessForm(props) {
+    console.log(props)
+
     return (
-        <form>
+        <form onSubmit={(event) => {
+            event.preventDefault()
+            let inputValue = event.target.userGuess.value;
+            props.newGuess(inputValue)
+        }}>
             <input type="text" name="userGuess" id="userGuess"
                 className="text" maxLength="3" autoComplete="off"
-                placeholder="Enter your Guess" required />
+                placeholder="Enter your Guess" 
+                // ref={input => this.textInput = input} 
+                required />
             <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
         </form>
     );
