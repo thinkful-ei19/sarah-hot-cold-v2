3ad012314d3e55c7d59889ad5dadc63e712c8807
ssheaks <sarah.sheaks@gmail.com>
2018-04-12 11:07:24 -0700

    implement guess feedback

--- src/components/game.js
+++ src/components/game.js
@@ -9,13 +9,37 @@ class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            guesses: []
+            guesses: [],
+            answer: Math.floor((Math.random() * 100) + 1),
+            feedback: 'Make your guess!'
         }
+        console.log(this.state.answer)
     }
 
+    
+
     makeGuess(number) {
+        const difference = Math.abs(number - this.state.answer);
+        console.log(difference);
+
+        let feedback;
+        if (difference > 60) {
+            feedback = 'Ice cold';
+        } else if (difference > 30) {
+            feedback = 'Cold'
+        } else if (difference > 10) {
+            feedback = 'Warm'
+        } else if (difference > 5) {
+            feedback = 'Hot'
+        } else if (difference > 1) {
+            feedback = 'On fire!!'
+        } else {
+            feedback = 'You guessed it!!! Click NEW GAME to play again'
+        }
+
         this.setState({
-            guesses: [...this.state.guesses, number]
+            guesses: [...this.state.guesses, number],
+            feedback
         });
     }
 
@@ -23,8 +47,8 @@ class Game extends React.Component {
     return (
         <div>
             <Header />
-            <GuessSection feedback="Make your guess!" add={(value) => this.makeGuess(value)} />
-            <GuessCount count={this.state.count} />
+            <GuessSection feedback={this.state.feedback} add={(value) => this.makeGuess(value)} />
+            <GuessCount count={this.state.guesses.length} />
             <GuessList guesses={this.state.guesses} />
         </div>
         );
